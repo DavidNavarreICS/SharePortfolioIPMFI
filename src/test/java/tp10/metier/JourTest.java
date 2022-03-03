39a63d08e39dc0f71a7900c6daee68153a90099b
David Navarre <David.Navarre@irit.fr>
2022-03-03 16:36:14 +0100

    End of pom.xml configuration

---
+++ src/test/java/tp10/metier/JourTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package tp10.metier;
+
+/**
+ *
+ * @author David Navarre <David.Navarre@irit.fr>
+ */
+public class JourTest {
+
+    public JourTest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSomeMethod() {
+    }
+
+}
